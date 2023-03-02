98967e6a2d320b9c0d9428dfffa6e4eb54b55b16
hs96wings <hs96wings@gmail.com>
2023-03-02 14:52:46 +0900

    1단계

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,24 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner scanner;
+
+    public App(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void run() {
+        System.out.println("=== 명언 앱 ===");
+
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine().trim();
+
+            if (cmd.equals("종료")) {
+                break;
+            }
+        }
+    }
+}
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,13 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        new App(scanner).run();
+
+        scanner.close();
     }
 }
