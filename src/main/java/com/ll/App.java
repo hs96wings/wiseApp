846a761c550e0e1df5da8ddad4dedcfef6e2a8d9
hs96wings <hs96wings@gmail.com>
2023-03-02 14:54:06 +0900

    2단계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -18,6 +18,11 @@ public class App {
 
             if (cmd.equals("종료")) {
                 break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = scanner.nextLine().trim();
+                System.out.print("작가 : ");
+                String author = scanner.nextLine().trim();
             }
         }
     }
