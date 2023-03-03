9889cb575c7a3361105ee231ba99505150a7ed40
hs96wings <hs96wings@gmail.com>
2023-03-03 10:29:53 +0900

    리포지터리 도입

--- src/main/java/com/ll/wiseSaying/Service/WiseSayingService.java
+++ src/main/java/com/ll/wiseSaying/Service/WiseSayingService.java
@@ -2,50 +2,37 @@ package com.ll.wiseSaying.Service;
 
 import com.ll.Rq;
 import com.ll.wiseSaying.entity.WiseSaying;
+import com.ll.wiseSaying.repository.WiseSayingRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class WiseSayingService {
-    private long lastWiseId;
-    private List<WiseSaying> wiseSayings;
+    private final WiseSayingRepository wiseSayingRepository;
+
 
     public WiseSayingService() {
-        wiseSayings = new ArrayList<>();
-        lastWiseId = 0;
-    }
-
-    public WiseSaying findById(long id) {
-        for (WiseSaying wiseSaying : wiseSayings) {
-            if (wiseSaying.getId() == id) {
-                return wiseSaying;
-            }
-        }
-
-        return null;
+        wiseSayingRepository = new WiseSayingRepository();
     }
 
     public List<WiseSaying> findAll() {
-        return wiseSayings;
+        return wiseSayingRepository.findAll();
+    }
+
+    public WiseSaying findById(long id) {
+        return wiseSayingRepository.findById(id);
     }
 
     public long write(String content, String author) {
-        long id = lastWiseId + 1;
-
-        WiseSaying wiseSaying = new WiseSaying(id, content, author);
-        wiseSayings.add(wiseSaying);
-
-        lastWiseId = id; // 방금 새 명언이 생성되었으니, lastWiseId 값을 갱신한다
-
-        return id;
+        return wiseSayingRepository.write(content, author);
     }
 
     public void remove(WiseSaying wiseSaying) {
-        wiseSayings.remove(wiseSaying);
+        wiseSayingRepository.remove(wiseSaying);
     }
 
     public void modify(WiseSaying wiseSaying, String content, String author) {
-        wiseSaying.setAuthor(author);
-        wiseSaying.setContent(content);
+        wiseSayingRepository.modify(wiseSaying, content, author);
     }
+
 }
--- src/main/java/com/ll/wiseSaying/repository/WiseSayingRepository.java
+++ src/main/java/com/ll/wiseSaying/repository/WiseSayingRepository.java
@@ -1,4 +1,50 @@
 package com.ll.wiseSaying.repository;
 
+import com.ll.wiseSaying.entity.WiseSaying;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WiseSayingRepository {
+    private long lastWiseId;
+    private List<WiseSaying> wiseSayings;
+
+    public WiseSayingRepository() {
+        wiseSayings = new ArrayList<>();
+        lastWiseId = 0;
+    }
+
+    public WiseSaying findById(long id) {
+        for (WiseSaying wiseSaying : wiseSayings) {
+            if (wiseSaying.getId() == id) {
+                return wiseSaying;
+            }
+        }
+
+        return null;
+    }
+
+    public List<WiseSaying> findAll() {
+        return wiseSayings;
+    }
+
+    public long write(String content, String author) {
+        long id = lastWiseId + 1;
+
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayings.add(wiseSaying);
+
+        lastWiseId = id; // 방금 새 명언이 생성되었으니, lastWiseId 값을 갱신한다
+
+        return id;
+    }
+
+    public void modify(WiseSaying wiseSaying, String content, String author) {
+        wiseSaying.setContent(content);
+        wiseSaying.setAuthor(author);
+    }
+
+    public void remove(WiseSaying wiseSaying) {
+        wiseSayings.remove(wiseSaying);
+    }
 }
